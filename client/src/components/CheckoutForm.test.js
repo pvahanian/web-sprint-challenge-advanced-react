e9fb417fdb88cfd614e08d4b83bd510f092703ee
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-10-02 12:33:02 -0700

    One test left

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", async () => { 
+    
+    render(<CheckoutForm />)
+
+    const header = screen.getAllByText(/Checkout Form/i)
+    console.log(header)
+    expect(header.values === 'Checkout Form').toBeTruthy()  
+
+});
+
 
 test("form shows success message on submit with form details", () => {});
+
+
